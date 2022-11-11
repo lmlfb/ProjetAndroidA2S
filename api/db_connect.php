829c77a732008f1e0b2feac94f7229014755175a
Le Floch Louis-Marie <lmleflochb@gmail.com>
2022-11-11 15:14:06 +0100

    added select chapter by id

---
+++ api/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $db = "enonce";
+  $conn = mysqli_connect($server, $username, $password, $db);
+?>
+
---
+++ api/get_all_question.php
@@ -0,0 +1,53 @@
+<?php
+try {
+$servername = "apiapkslm.mysql.db";
+$username = "apiapkslm";
+$password = "MWcYrxMarR9vdU9NGmf";
+$dbname = "apiapkslm";
+
+$base  = new mysqli($servername, $username, $password, $dbname);
+$sql = "select * from enonce";
+$return_arr = array();
+
+if ($result = mysqli_query( $base, $sql )){
+    while ($row = mysqli_fetch_assoc($result)) {
+    $row_array['id'] = $row['id'];
+    $row_array['titre'] = $row['titre'];
+
+
+    array_push($return_arr,$row_array);
+   }
+ }
+ echo json_encode($return_arr);
+mysqli_close($base);
+
+} catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+  echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+  echo "Exception Msg: " . $e->getMessage();
+  exit(); // exit and close connection.
+}
+
+
+/*
+try {
+  // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+  $mysqli = new mysqli($servername, $username, "", $dbname);
+
+  
+  $statement = $mysqli->prepare("select * from enonce");
+
+
+  $statement->execute(); // Execute the statement.
+  $result = $statement->get_result(); // Binds the last executed statement as a result.
+
+  echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+
+} catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+  echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+  echo "Exception Msg: " . $e->getMessage();
+  exit(); // exit and close connection.
+}
+
+$mysqli->close(); */
+
+?>
