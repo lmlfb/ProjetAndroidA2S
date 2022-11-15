87332f6d5ad8f6fbd9de4a5f3a0921556c419f56
Axel Frauchot <axel.frauchot@gmail.com>
2022-11-15 12:12:38 +0100

    SPLASH_SCREEN

---
+++ app/src/main/java/com/a2s/mvvv/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.a2s.mvvv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Redirection vers la page principale "MainActivity" après 3 secondes.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Démarrage de la page
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
