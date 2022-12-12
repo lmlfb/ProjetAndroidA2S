e814daee581e17f331c605c9514f3f4c80c7c8a7
Axel Frauchot <axel.frauchot@gmail.com>
2022-12-12 13:55:02 +0100

    Ajout Design chapitre

---
+++ app/src/main/java/com/a2s/mvvv/one_chapitre.java
@@ -0,0 +1,14 @@
+package com.a2s.mvvv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class one_chapitre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_chapitre);
+    }
+}
